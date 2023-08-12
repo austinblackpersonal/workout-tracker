4506efb359749fc5696f69d9fcf209365858fe05
austinblackpersonal <austin.black.55@gmail.com>
2023-08-12 09:22:06 -0500

    finished home page styling for now

--- workout-tracker/src/views/Home.jsx
+++ workout-tracker/src/views/Home.jsx
@@ -1,4 +1,5 @@
 import '../styles/Home.css'
+import { Typography } from '@mui/material';
 import Navbar from "./partials/Navbar";
 
 export default function Home() {
@@ -7,8 +8,8 @@ export default function Home() {
         <div>
             <Navbar/>
             <div className="Home">
-                <h1>Get the Body of your Dreams&trade;</h1>
-                <p>Tired of working out all the time but not making progress?! Here you can write down your workouts, that way, you know how much to lift next time! Can't do that anywhere else...</p>
+                <Typography variant='h1'>Get the Body of your Dreams&trade;</Typography>
+                <Typography variant='h6'>Tired of working out all the time but not making progress?! Here you can write down your workouts, that way, you know how much to lift next time! Can't do that anywhere else...</Typography>
             </div>
 
         </div>
