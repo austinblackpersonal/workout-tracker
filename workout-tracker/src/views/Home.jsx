14efb8fb5b574796e0104e79641e73fd4c7904d0
austinblackpersonal <austin.black.55@gmail.com>
2023-08-11 20:48:02 -0500

    added material ui and styles, started work on home page

---
+++ workout-tracker/src/views/Home.jsx
@@ -0,0 +1,16 @@
+import '../styles/Home.css'
+import Navbar from "./partials/Navbar";
+
+export default function Home() {
+
+    return (
+        <div>
+            <Navbar/>
+            <div className="Home">
+                <h1>Get the Body of your Dreams&trade;</h1>
+                <p>Tired of working out all the time but not making progress?! Here you can write down your workouts, that way, you know how much to lift next time! Can't do that anywhere else...</p>
+            </div>
+
+        </div>
+    )
+};
